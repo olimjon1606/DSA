25e236d2a55706efede018b2d8e719067311d41b
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-05 00:05:38 +0200

    Update app.js

--- Number of Islands Solution/app.js
+++ Number of Islands Solution/app.js
@@ -1,33 +1,50 @@
-grid[]
-let numIlslands = function (grid) {
+const grid = [
+    [1, 1, 1, 1, 0],
+    [1, 1, 0, 1, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0]
+];
+/*
+ * @param {character[][]} grid
+ * @return {number}
+ */
+function numIslands(grid) {
+    console.log("hello")
+
     if (!grid || grid.length === 0) {
-        return 0
+        return 0;
     }
-    const m = grid.length
-    const n = grid[0].length
-    let count = 0
+    console.log("help");
+
+    const m = grid.length;
+    const n = grid[0].length;
+    console.log("hello")
+
+    let count = 0;
 
     const dfs = (i, j) => {
         if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === '0') {
             return;
         }
 
-        grid[i][j] = '0'
+        grid[i][j] = '0';
 
-        dfs(i - 1, j)
-        dfs(i + 1, j)
-        dfs(i, j - 1)
-        dfs(i, j + 1)
-    }
+        dfs(i - 1, j);
+        dfs(i + 1, j);
+        dfs(i, j - 1);
+        dfs(i, j + 1);
+    };
 
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             if (grid[i][j] === '1') {
-                count++
-                dfs(i, j)
+                count++;
+                dfs(i, j);
             }
         }
     }
 
-    return count
-}
+    return count;
+};
+console.log(numIslands());
+console.log(!grid);
