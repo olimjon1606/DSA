033c4e8da51a9fe4abc99a60cd36187923cfc284
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-06 00:25:46 +0200

    s

---
+++ Reversed LinkedList/app.js
@@ -0,0 +1,37 @@
+
+
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+const node5 = new Node(5);
+const node4 = new Node(4, node5);
+const node3 = new Node(3, node4);
+const node2 = new Node(2, node3);
+const node1 = new Node(1, node2);
+
+console.log(reverseLinkedList(node1)); // Output: Node { value: 5, next: Node { value: 4, next: Node { value: 3, next: Node { value: 2, next: Node { value: 1, next: null } } } } } 
+
+
+
+
+function reverseLinkedList(head) {
+    let current = head;
+    let prev = null;
+    let next = null;
+    // console.log(current.next)
+
+    while (current !== null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
+// let head = [1, 2, 3, 4, , 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log(reverseLinkedList(Node))
