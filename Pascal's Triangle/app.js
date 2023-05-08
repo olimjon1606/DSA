704b91b5fc482829b7739d24f93efcb599ccd1e1
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-08 22:30:51 +0200

    Update app.js

--- Pascal's Triangle/app.js
+++ Pascal's Triangle/app.js
@@ -2,11 +2,11 @@
 var generate = function(numRows) {
     const triangle = [];
     for (let i = 0; i < numRows; i++) {
-        const row = Array(i + 1).fill(1); // initialize current row with 1s
+        const row = Array(i + 1).fill(1);
         for (let j = 1; j < i; j++) {
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
         }
-        triangle.push(row); // add the current row to the triangle
+        triangle.push(row);
     }
     return triangle;
 };
