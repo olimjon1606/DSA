39cc36da3ce707f2dd57e78e2454b82cf27b3a09
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-08 22:30:00 +0200

    Update app.js

--- Pascal's Triangle/app.js
+++ Pascal's Triangle/app.js
@@ -4,7 +4,6 @@ var generate = function(numRows) {
     for (let i = 0; i < numRows; i++) {
         const row = Array(i + 1).fill(1); // initialize current row with 1s
         for (let j = 1; j < i; j++) {
-            // compute the value of each element in the current row based on the previous row
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
         }
         triangle.push(row); // add the current row to the triangle
