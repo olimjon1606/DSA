32f13d6382cbccab6217849c27026fb1fe4804aa
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-12 22:57:56 +0200

    Update app.js

---
+++ Contains Duplicate/app.js
@@ -0,0 +1,13 @@
+
+const array = [1, 2, 3, 4, 5];
+const duplicates = function (array) {
+    let double = false
+    for (i = 0; i < array.length; i++) {
+        if(array.indexOf(array[i]) !== i)
+        {
+            double = true
+        }
+    }
+    return double
+}
+console.log(duplicates(array));
