73478e0afc257a25acad4e8c15f4c311dda6ff96
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-14 20:27:17 +0200

    Update Linked_List.js

---
+++ Linked_List.js
@@ -0,0 +1,39 @@
+function isPalindrome(head) {
+    // Base case: empty or single-node list is always a palindrome
+    if (!head || !head.next) {
+      return true;
+    }
+  
+    // Find the middle node of the list
+    let slow = head;
+    let fast = head;
+    while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+  
+    // Reverse the second half of the list
+    let prev = null;
+    let curr = slow.next;
+    while (curr) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+  
+    // Compare the first and second halves of the list
+    let p1 = head;
+    let p2 = prev;
+    while (p2) {
+      if (p1.val !== p2.val) {
+        return false;
+      }
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+  
+    // If we reach this point, the list is a palindrome
+    return true;
+  }
+  
